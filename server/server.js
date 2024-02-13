9608189c94103b43895f2520029cdc1e8d6c1927
TariqKichawele <tariqkichawele66@gmail.com>
2024-02-13 08:27:16 -0500

    final changes

--- server/server.js
+++ server/server.js
@@ -5,22 +5,30 @@ import authRoutes from './routes/AuthRoute.js';
 import messageRoutes from './routes/MessageRoute.js';
 import userRoutes from './routes/UserRoute.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
+import path from 'path';
 import { app, server }from './socket/socket.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3005;
 
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
 server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server is running on port ${PORT}`);
 });
 
-app.use(express.json());
-app.use(cookieParser());
-app.use('/api/auth', authRoutes);
-app.use('/api/messages', messageRoutes);
-app.use('/api/users', userRoutes);
-app.use('/' , (req, res) => {
-  res.send('Server is running');
-});
